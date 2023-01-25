1aa097b7de20ae6786377f4a3bddf3889a4bf4b4
Federico Lopez <arq.lopezfederico@gmail.com>
2023-01-25 18:43:07 -0300

    25.01.23-Ajustes en JS formulario.

---
+++ 02_JS/sintaxis.js
@@ -0,0 +1,14 @@
+function contadorespacios(obj){
+    var max_espacios=299;
+    var strlength=obj.value.length
+
+    if(strlength > max_espacios){
+        document.getElementById("contadorch").innerHTML = "sin espacios restantes";
+    }
+    else{
+        document.getElementById("contadorch").innerHTML = strlength + "/300";
+    }
+
+    
+}
+
